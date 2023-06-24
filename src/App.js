9e3ce82682bd7d3452e53aedb8784c7d41ad84f7
Prajwal <prajwalkulkarni766@gmail.com>
2023-06-24 17:33:17 +0530

    Started developing application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      hello
+    </>
+  );
+}
+
+export default App;
